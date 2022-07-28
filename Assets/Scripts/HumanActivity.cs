73c2f5303f86a64de9c51a9ff1ffba37f4a148d6
Leo Ji <sj3026@nyu.edu>
2022-07-28 14:51:51 -0400

    naughty attribute

--- Assets/Scripts/HumanActivity.cs
+++ Assets/Scripts/HumanActivity.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using NaughtyAttributes;
 
 public enum ActivityClass{Working, Middle, Upper, Company, Policymaker}
 
 [Serializable]
 public class HumanActivity
 {
-    private string activityName;
-    private int startYear, lastEndYear, duration;
-    private ActivityClass classType;
+    [SerializeField] private string activityName;
+    [SerializeField] private List<int> startYears, endYears;
+    private int duration = 0;
+    [SerializeField] private ActivityClass classType;
 
     //getters & setters
     public string ActivityName {get=>activityName;private set=>activityName=value;}
-    public int StartYear {get=>startYear; set=>startYear=value;}
-    public int LastEndYear {get=>lastEndYear; set=>lastEndYear=value;}
-    public int Duration {get=>duration;private set=>duration=value;}
+    public List<int> StartYears {get=>startYears;set=>startYears=value;}
+    public List<int> EndYears {get=>endYears;set=>endYears=value;}
+    public int Duration {get=>duration;set=>duration=value;}
     public ActivityClass ClassType{get=>classType;private set=>classType=value;}
 }
